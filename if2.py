d4f1c98871793ea9c08cce70ab4846144fb381a2
Erendgen <erendgen@phystech.edu>
2019-09-14 12:00:49 +0300

    Доработка if2.py

--- if2.py
+++ if2.py
@@ -1,8 +1,5 @@
 def main(str1, str2):        
-    try: 
-        float(str1) #пытаемся привести введенное значение к числу. Приведение к bool не имеет смысла.
-        float(str2) #пытаемся привести введенное значение к числу. Приведение к bool не имеет смысла.        
-    except ValueError:
+    if isinstance(str1, str) and isinstance(str2, str):
         if str1 == str2:
             return 1
         elif str1 != str2 and len(str1) > len(str2):
@@ -10,9 +7,22 @@ def main(str1, str2):
         elif str1 != str2 and str2 == 'learn':
             return 3
         else:
-            return 'Обе переменные строки, но не попадают под условия задачи'
+            return '"В задаче не описано что возвращать"'
     else:
         return 0
-str1 = input('Введите первую строку: ')
-str2 = input('Введите вторую строку: ')
-print(main(str1, str2))
+
+str1 = 'QWE'
+str2 = 'QWE'
+print('Строки одинаковые, печатаем ' + str(main(str1, str2)))
+str1 = 'QWE34'
+str2 = 'QWE'
+print('Строки разные и первая строка длиннее, печатаем ' + str(main(str1, str2)))
+str1 = 'QWE34'
+str2 = 'learn'
+print('Строки разные и вторая строка "learn", печатаем ' + str(main(str1, str2)))
+str1 = 1
+str2 = 'asdasd'
+print('Один из аргументов функции не строка, печатаем ' + str(main(str1, str2)))
+str1 = 'sad'
+str2 = 'asdasd'
+print('Не описанный случай в задаче: первая строка короче второй, печатаем ' + str(main(str1, str2)))
